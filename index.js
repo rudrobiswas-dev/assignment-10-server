3ca69f74c591e5562809f087c84a327db2f59649
rudrobiswas-dev <rudrobiswas11023@gmail.com>
2026-08-04 01:51:58 +0600

    add fi

--- index.js
+++ index.js
@@ -572,6 +572,10 @@ app.get("/", (req, res) => {
   res.send("Server is running fine!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
